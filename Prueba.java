1229e57b4e7ffa59962cacdda93afbf03def07a1
Adrian <adrianpalomofe@gmail.com>
2024-09-26 10:57:00 +0200

    Añadiendo tambien red social fav.

--- Prueba.java
+++ Prueba.java
@@ -2,5 +2,6 @@ public class Prueba {
 	public static void main(String[] args) {
 		System.out.println("Atletismo");
 		System.out.println("Republica Dominicana");
+		System.out.println("Red social fav: Instagram");
 	}
 }
